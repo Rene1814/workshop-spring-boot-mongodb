6e8f96614c1ffe2666c8af8438426d6a4dea40a8
Rene1814 <rne_souza@yahoo.com.br>
2023-07-29 16:03:41 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/renesouza/workshopmongo/domain/User.java
+++ src/main/java/com/renesouza/workshopmongo/domain/User.java
@@ -3,10 +3,15 @@ package com.renesouza.workshopmongo.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user") //ess anotação indica que a classe se trata de uma coleção (tabela) do mongo db
 public class User implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private String id;
 	private String name;
 	private String email;
