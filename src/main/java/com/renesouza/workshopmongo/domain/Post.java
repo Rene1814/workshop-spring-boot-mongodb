06ccac8183f5ae0c2ac957ec105967ce33e515eb
Rene1814 <rne_souza@yahoo.com.br>
2023-07-30 10:38:41 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/renesouza/workshopmongo/domain/Post.java
@@ -0,0 +1,91 @@
+package com.renesouza.workshopmongo.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Post implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private String id;
+	private Date date;
+	private String tilte;
+	private String body;
+	private User author;
+	
+	public Post() {
+		
+	}
+
+	public Post(String id, Date date, String tilte, String body, User author) {
+		super();
+		this.id = id;
+		this.date = date;
+		this.tilte = tilte;
+		this.body = body;
+		this.author = author;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getTilte() {
+		return tilte;
+	}
+
+	public void setTilte(String tilte) {
+		this.tilte = tilte;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+	
+	public User getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Post other = (Post) obj;
+		return Objects.equals(id, other.id);
+	}
+}
