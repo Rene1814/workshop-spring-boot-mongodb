8587f1bfcbd62eacce3dc6719f5f79e61527348b
Rene1814 <rne_souza@yahoo.com.br>
2023-08-03 20:59:46 -0300

    Consulta simples com query methods

--- src/main/java/com/renesouza/workshopmongo/domain/Post.java
+++ src/main/java/com/renesouza/workshopmongo/domain/Post.java
@@ -20,7 +20,7 @@ public class Post implements Serializable{
 	@Id
 	private String id;
 	private Date date;
-	private String tilte;
+	private String title;
 	private String body;
 	private AuthorDTO author;
 	
@@ -30,11 +30,11 @@ public class Post implements Serializable{
 		
 	}
 
-	public Post(String id, Date date, String tilte, String body, AuthorDTO author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
 		this.date = date;
-		this.tilte = tilte;
+		this.title = title;
 		this.body = body;
 		this.author = author;
 	}
@@ -55,12 +55,12 @@ public class Post implements Serializable{
 		this.date = date;
 	}
 
-	public String getTilte() {
-		return tilte;
+	public String getTitle() {
+		return title;
 	}
 
-	public void setTilte(String tilte) {
-		this.tilte = tilte;
+	public void setTitle(String title) {
+		this.title = title;
 	}
 
 	public String getBody() {
